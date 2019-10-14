ea1aae893cffbc9e0b966888b0c8e37ac4643544
Christina Truong <christinaqtruong@gmail.com>
2019-10-14 12:37:35 -0500

    successfully installed and used react-bootstrap

---
+++ portfolio/src/components/title.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+function Title () {
+  
+    return (
+      <Jumbotron fluid>
+          <h1>christina q. truong</h1>
+          <p>
+            Full stack web developer specialized in React and Javascript.
+          </p>
+      </Jumbotron>
+    );
+  
+}
+
+export default Title;
