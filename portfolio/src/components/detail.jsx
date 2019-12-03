6deb03cd74d4c0a3a84c0c2abab3b19932dfd01a
Christina Truong <christinaqtruong@gmail.com>
2019-12-02 22:03:33 -0600

    created detail scss

--- portfolio/src/components/detail.jsx
+++ portfolio/src/components/detail.jsx
@@ -1,23 +1,25 @@
 import React, { useContext } from "react";
 import { DataContext } from "./data";
+import "../styles/detail.scss";
 
-const Detail = (props) => {
+const Detail = props => {
   const [projects] = useContext(DataContext);
   let { id } = props.match.params;
 
   let project = projects.find(project => {
     return project.slug === id;
-  })
+  });
+
+  console.log(project.dev);
 
   return (
-    <div
-      className="wrapper"
-      style={{
-        display: "flex",
-        justifyContent: "center"
-      }}
-    >
+    <div className="wrapper">
       <h1>{project.title}</h1>
+      <ul className="dev">
+        {project.dev.map(dev => (
+          <li>{dev}</li>
+        ))}
+      </ul>
     </div>
   );
 };
